6db3676796a34598973d040c408edf5a62ba35c1
TomoyaKishimoto <65524873+TomoyaKishimoto@users.noreply.github.com>
2023-03-03 16:41:45 +0900

    Add files via upload

--- PulseSensor/4/4.ino
+++ PulseSensor/4/4.ino
@@ -1,17 +1,18 @@
+#include <Servo.h>
 #define USE_ARDUINO_INTERRUPTS true    // BPMの計算用の設定、おまじない
 #include <PulseSensorPlayground.h>     // パルスセンサーのライブラリをインクルードする
-#include <PWMServo.h>
 
 int pulsePin = A0;    // パルスセンサの入力ピンをA0にする
 int LEDPin = 13;      // LEDのピンを13番にする
 int pulseVal;         // パルスセンサの値を格納する変数 
-int threshold = 525;  // 心拍のしきい値、pulseValがこれより大きいとき心拍したとする
+int threshold = 550;  // 心拍のしきい値、pulseValがこれより大きいとき心拍したとする
 
 PulseSensorPlayground pulseSensor; // パルスセンサーの変数を用意 (パルスセンサーのインスタンス生成)
 int myBPM;            // BPM(心拍数)を格納する変数
 
-PWMServo myServo;
+Servo myServo;
 int servoPin = 9;
+int pos;
 
 void setup() {
   pinMode(LEDPin, OUTPUT); // LEDPinを出力に設定する
@@ -23,20 +24,21 @@ void setup() {
   pulseSensor.begin();                 // 計測開始
 
   myServo.attach(servoPin);
-}
+} 
 
 void loop() {
+  //pulseVal = analogRead(pulsePin);
+  //Serial.println(pulseVal);
   myBPM = pulseSensor.getBeatsPerMinute();  // PulseSensorPlaygroundライブラリ内のgetBeatsPerMinute()関数を呼び出し、BPMを計算しmyBPMに格納.
   if (pulseSensor.sawStartOfBeat()) {       // もし心拍が起きたならば
     Serial.print("BPM: ");                  // シリアルモニタに "BPM: " を表示
     Serial.println(myBPM);                  // シリアルモニタにmyBPMの中身を表示
-  }
 
-  if(myBPM > 75){
-    myServo.write(160);
-  }
-  else{
-    myServo.write(20);
+    if(myBPM < 180){
+      pos = map(myBPM, 0, 180, 20, 160);
+    }
+    Serial.println(pos);
+    myServo.write(pos);
   }
 
   delay(20);
